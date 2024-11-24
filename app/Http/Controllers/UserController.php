77eb0c806143ef0daf5c118a607da6eecb815382
Kareem-Bizreh <kareembezreh@gmail.com>
2024-11-24 20:41:08 +0300

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+/**
+ * @OA\Info(title="My API Docs", version="0.1")
+ */
+
+class UserController extends Controller
+{
+    //
+}
